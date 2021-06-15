a08bee3bbece379fc57f1a288f3b03e437239c22
Amiel Luis Labajo <38656969+yatootz23@users.noreply.github.com>
2021-06-15 10:49:40 +0800

    Add files via upload

--- web_browser/website/pages/forms.py
+++ web_browser/website/pages/forms.py
@@ -1,9 +1,16 @@
 from django.contrib.auth.forms import UserCreationForm
 from django import forms
 from django.contrib.auth.models import User
+from .models import History
 
 
 class CreateUserForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+
+
+class HistoryForm(forms.ModelForm):
+    class Meta:
+        model = History
+        fields = '__all__'
